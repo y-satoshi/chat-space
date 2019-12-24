5868147e6482f6dbbe5c9d56c39413ae411c8c7b
y-satoshi <57346310+y-satoshi@users.noreply.github.com>
2019-12-24 16:35:43 +0900

    自動デプロイ

---
+++ config/deploy.rb
@@ -0,0 +1,26 @@
+# config valid only for current version of Capistrano
+lock '3.11.2'
+
+set :application, 'chat-space'
+
+set :repo_url,  'git@github.com:y-satoshi/chat-space.git'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '5.2.1'
+
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/chatspace_pem.pem']
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
